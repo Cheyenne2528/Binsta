cf92e3afe9435f24f792ed4bb4966e9b83d155e8
Apple <apple@Apples-MacBook-Pro-8.local>
2019-07-30 13:38:14 -0400

    Complete Iteration 2

--- Binsta/BinstaViewController.swift
+++ Binsta/BinstaViewController.swift
@@ -12,6 +12,18 @@ class BinstaViewController: UIViewController, UIImagePickerControllerDelegate, U
 {
     var imagePicker = UIImagePickerController()
 
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        {
+            newImageView.image = selectedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+
+    
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -19,7 +31,7 @@ class BinstaViewController: UIViewController, UIImagePickerControllerDelegate, U
 
         // Do any additional setup after loading the view.
     }
-    
+
   
     @IBAction func takeSelfieTapped(_ sender: Any) {
         imagePicker.sourceType = .camera
@@ -42,9 +54,34 @@ class BinstaViewController: UIViewController, UIImagePickerControllerDelegate, U
         present(imagePicker, animated:true, completion: nil)
     }
     
+    
+    @IBAction func savePhotoTapped(_ sender: UIButton)
+    {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+        {
+            let photoToSave = Photos(entity: Photos.entity(), insertInto: context)
+            photoToSave.caption = captionText.text
+            
+            if let userImage = newImageView.image {
+            
+                if let userImageData = userImage.pngData()
+                {
+                    photoToSave.imageData = userImageData
+                }
+            }
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        navigationController?.popViewController(animated: true)
+        }
 
+    }
+    
     
 
+    @IBOutlet weak var captionText: UITextField!
+
+    
+    @IBOutlet weak var newImageView: UIImageView!
+    
     /*
     // MARK: - Navigation
 
